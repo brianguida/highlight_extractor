b77ef84d90a37be46c628e930a9fe70e02cfbc55
brianguida <73550637+brianguida@users.noreply.github.com>
2025-04-08 04:01:15 -0400

    Initial commit with spike detector and clip extractor

---
+++ audio_spike_detector.py
@@ -0,0 +1,55 @@
+#Detects spikes in audio for highlight detection
+#imports
+import librosa
+import numpy as np
+import moviepy.editor as mp
+import json
+import matplotlib.pyplot as plt
+
+#Temporary config for testing
+VIDEO_PATH = "Grez_hits_pl3.mp4"
+OUTPUT_JSON = "spike_timestamps.json"
+THRESHOLD = 0.04
+
+#Extract audio from mp4 file
+print("Loading video and extracting audio")
+video = mp.VideoFileClip(VIDEO_PATH)
+audio_path = "temp_audio.wav"
+video.audio.write_audiofile(audio_path, verbose = False, logger = None)
+
+#Analyze extracted audio
+print("Analyzing extracted audio")
+y, sr = librosa.load(audio_path)
+frame_length = 2048
+hop_length = 512
+
+rms = librosa.feature.rms(y=y, frame_length=frame_length, hop_length=hop_length)[0]
+times = librosa.frames_to_time(range(len(rms)), sr=sr, hop_length=hop_length)
+
+#Logic to find spikes in audio
+#rms needs to be above THRESHOLD
+#rms needs to be larger than previous frame
+spikes = []
+for i in range(1, len(rms)):
+    if rms[i] > THRESHOLD and rms[i] > rms[i-1] * 1.5:
+        spikes.append(times[i])
+        
+print(f"found {len(spikes)} spikes.")
+print(f"Saving to {OUTPUT_JSON}")
+
+#Save spikes
+with open(OUTPUT_JSON, "w") as f:
+    json.dump(spikes, f, indent= 2)
+    
+#Visualize audio spikes
+plt.figure(figsize=(14, 5))
+plt.plot(times, rms, label="RMS Energy")
+plt.axhline(y=THRESHOLD, color='r', linestyle='--', label=f"Threshold = {THRESHOLD}")
+for spikes in spikes:
+    plt.axvline(x=spikes, color='g', linestyle=':', alpha=0.6)
+plt.title("Audio RMS with Spike Detection")
+plt.xlabel("Time (s)")
+plt.ylabel("RMS Energy")
+plt.legend()
+plt.tight_layout()
+plt.show()
---
+++ clip_extractor.py
@@ -0,0 +1,34 @@
+#extracts clips based on spike_timestamps.json
+#import
+import json
+import os
+from moviepy.editor import VideoFileClip
+
+#temporary config
+VIDEO_PATH = "Grez_hits_pl3.mp4"
+TIMESTAMPS_FILE = "spike_timestamps.json"
+OUTPUT_DIR = "highlights"
+CLIP_LENGTH = 10
+
+#sanity check for output folder
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+#load spike_timestamps.json and video
+with open(TIMESTAMPS_FILE, "r") as f:
+    timestamps = json.load(f)
+video = VideoFileClip(VIDEO_PATH)
+half = CLIP_LENGTH / 2 #+- 5 seconds
+
+#extract clips
+for i, ts in enumerate(timestamps):
+    start = max(0, ts - half)
+    end = min(video.duration, ts + half)
+    
+    subclip = video.subclip(start, end)
+    output_path = os.path.join(OUTPUT_DIR, f"highlight_{i+1:02}.mp4")
+    
+    print(f"Extracting and saving highlight {i+1} ({start:.2f}s to {end:.2f}s)...")
+    subclip.write_videofile(output_path, codec="libx264", audio_codec="aac",verbose=False, logger=None)
+    
+print("All highlights extracted and saved to /highlights")
+    
